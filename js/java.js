4d3d12966a1bd2ec3b5d57501a87b93759745e21
Opala <opala74@gmail.com>
2018-07-29 19:27:45 +0200

    All sections done

---
+++ js/java.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $('#window1').hover(function() {
+        $("#window1").addClass('window');
+    
+    }, function() {
+        $("#window1").removeClass('window');
+    });
+});
+$(document).ready(function(){
+    $('#window2').hover(function() {
+        $("#window2").addClass('window');
+    
+    }, function() {
+        $("#window2").removeClass('window');
+    });
+});
+$(document).ready(function(){
+    $('#window3').hover(function() {
+        $("#window3").addClass('window');
+    
+    }, function() {
+        $("#window3").removeClass('window');
+    });
+});
+$(document).ready(function(){
+    $('#window4').hover(function() {
+        $("#window4").addClass('window');
+    
+    }, function() {
+        $("#window4").removeClass('window');
+    });
+});
